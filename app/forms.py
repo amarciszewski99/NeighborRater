62faf81f91ae72448c0e7392a4e8c7cb9a0628e0
Adam <amarciszewski99@gmail.com>
2023-05-02 17:22:48 -0400

    improved field validation for login

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PassWordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, EqualTo
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired()])
-    password = PassWordField('Password', validators=[DataRequired()])
-    password2 = PassWordField('Retype Password', validators=[DataRequired(), EqualTo('password')])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Retype Password', validators=[DataRequired(), EqualTo('password')])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
 
 
 @app.route('/')
@@ -11,6 +12,6 @@ def index():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        flash('Login requested for email {}, remember_me={}'.format(form.email.data, form.remember_me.data))
         return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
