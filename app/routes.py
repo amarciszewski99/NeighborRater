47d975f16e7e0204591f89769547c85f4fb0c6af
Adam <amarciszewski99@gmail.com>
2023-05-02 18:30:31 -0400

    initial SQLite database created

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
@@ -12,6 +12,6 @@ def index():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for email {}, remember_me={}'.format(form.email.data, form.remember_me.data))
-        return redirect('/index')
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
