5a4fd33065a377cdf22aa7b8a3e885bb53246901
Adam <amarciszewski99@gmail.com>
2023-05-04 22:28:17 -0400

    login, logout, and registration added

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,24 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, EqualTo
+from wtforms.validators import DataRequired, EqualTo, Email
+from app.models import User
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
-    password2 = PasswordField('Retype Password', validators=[DataRequired(), EqualTo('password')])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+
+class RegistrationForm(FlaskForm):
+    first_name = StringField('First Name', validators=[DataRequired()])
+    last_name = StringField('Last Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email')
--- neighborrater.py
+++ neighborrater.py
@@ -1,1 +1,6 @@
-from app import app
+from app import app, db
+from app.models import User, Address, UserToAddress, Rating
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Address': Address, 'UserToAddress': UserToAddress, 'Rating': Rating}
